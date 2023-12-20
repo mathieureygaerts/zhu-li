ef07a9b084bd5f8cfa8ea8a18813aa8ac186d82e
Mathieu Reygaerts <mathieu@reygaerts.net>
2023-12-20 14:21:03 -0500

    Some refactoring and documentation

---
+++ README.md
@@ -0,0 +1,95 @@
+# Zhu li
+
+Zhu li is a super simple voice assistant. It publishes MQTT messages when commands are triggered.
+Those messages can then be used by something like Home Assistant to toggle lights.
+
+## Requirements
+
+|Softwares|Notes|
+|---|---|
+|Python 3|Tested with 3.10. It would most probably also work with 3.9.|
+|MQTT Broker|Tested with Mosquitto. You may already have an instance running if you use Zigbee2MQTT.|
+|Some kind of home automatization system|Tested with Home Assistant.|
+
+
+### Python requirements
+
+All the needed requirements are set in the requirements.txt file. I would suggest to install them in a new python environment.
+`pip install -r requirements.txt`
+
+### Environment variables
+
+Before running the script, you need to set a few environment variables: `VOSK_MODEL_PATH`, `MQTT_SERVER`.
+
+This script use Vosk, a speech recognition toolkit. It supports multiple languages and have small models than can run on a raspberry pi.
+Download the model you want from `https://alphacephei.com/vosk/`. 
+I tested this with the `vosk-model-small-en-us-0.15` model.
+
+If you placed the unzipped folder beside this script, the environment variable would look something like: `VOSK_MODEL_PATH=./vosk-model-small-en-us-0.15`
+
+
+#### Variable list
+|Name|Default|Description|
+|---|---|---|
+|`VOSK_MODEL_PATH`||Path to the Vosk Model|
+|`MQTT_SERVER`||Ip address of the MQTT broker|
+|`MQTT_PORT`|`1883`|Port|
+|`ASSISTANT_NAME`|`Zhu Li`|Name of the Assistant|
+|`MSG_ON_FAIL`|`False`|Send a MQTT message with the `fail` topic (e.g: `zhuli/fail`) on failing match.|
+
+
+## Set the voice commands
+
+You can set the voice commands by modifying the `commands.yml` file. A command is defined by a dictionary.
+
+```json
+{
+    "do the thing": {
+        "topic": "light",
+        "score": 0.72,
+        "payload": null
+    }
+}
+```
+
+The key defines the command. To activate it, you need to say the assistant's name first. e.g:
+> Zhu Li do the thing.
+
+The topic defines the name of the mqtt topic. It will always be prefixed by the assistant name first. e.g:
+`zhuli/light`
+
+The score defines how close the result of the voice command need to match the command to be triggered. 1 means it needs a perfect match, 0 means anything will trigger it.
+Set it too low and it will trigger all the time, set it to high and it will rarely trigger. Tweak the score in function of your speech and model.
+
+To help to determine a score that works for you, look at the logs when running the script. Say something and you will see the score it got for each commands. If the highest score is equal or above the score you set for that command, it will trigger.
+```text
+INFO:Zhu Li:Audio Process: julie do to thing
+INFO:Zhu Li:Similarity: Zhu Li do the thing: 0.8239422084623323 Zhu Li diningroom: 0.6131907308377896 Zhu Li bedroom: 0.6129785247432307 Zhu Li kitchen: 0.6129785247432307 Zhu Li thank you: 0.6044117647058824 Zhu Li energize: 0.5986928104575163 Zhu Li livingroom: 0.592156862745098 Zhu Li desk: 0.5874713521772345 Zhu Li romantica: 0.5491557734204793 Zhu Li entrance: 0.5429738562091503 Zhu Li laundry: 0.4943977591036415 
+INFO:Zhu Li:Zhu Li do the thing Triggered
+```
+
+The payload is there if you want to pass along extra data in the mqtt message. It should be a dictionary. 
+
+
+
+## MQTT message format
+
+The published topic will be the name of the assistant in lower case & without any space plus the topic value of the triggered command. e.g `zhuli/light`
+
+The message will include a json payload with the command that got triggered, the input it got from the voice & the score.
+```json
+{
+    "action": "Zhu li do the thing",
+    "input": "julie do to thing",
+    "score": 0.8239422084623323
+}
+```
+
+If you also set a payload in the `commands.yml` for that action, it will be added here.
+
+
+## Running
+
+Once all the above requirements are fulfilled, simply run the main.py script.
+
+You will need to set your home assistant to listen for the mqtt messages and trigger your devices accordingly.
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+"""
+Super simple voice assistant.
+It publishes MQTT messages when commands are triggered. 
+"""
 import sys
+import os
 from time import sleep
 import logging
 import json
@@ -8,52 +13,103 @@ from pyaudio import PyAudio, paInt16
 from jellyfish import jaro_winkler_similarity
 import paho.mqtt.client as mqtt
 
-MODEL = './vosk-model-small-en-us-0.15'
+MODEL = os.environ['VOSK_MODEL_PATH']
 
-MQTT_SERVER = '10.87.88.150'
-MQTT_PORT = 1883
-
-ASSISTANT_NAME = 'Zhu Li'
+MQTT_SERVER = os.environ['MQTT_SERVER']
+MQTT_PORT = int(os.environ.get('MQTT_PORT', '1883'))
+MSG_ON_FAIL = os.environ.get('MQTT_ON_FAIL', 'False').lower() == 'true'
+ASSISTANT_NAME = os.environ.get('ASSISTANT_NAME', 'Zhu Li')
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(ASSISTANT_NAME)
 
-Action = namedtuple('Action', 'topic score playload')
-Patern = namedtuple('Patern', 'input action score')
+def fetch_commands():
+    """Parse the commands.json file
 
-commands = {
-    'energize': Action('energize', 0.72, None),
-    'desk': Action('desk', 0.75, None),
-    'do the thing': Action('light', 0.72, None),
-    'romantica': Action('romantica', 0.72, None),
-    'bedroom': Action('bedroom', 0.72, None),
-    'livingroom': Action('livingroom', 0.72, None),
-    'kitchen': Action('kitchen', 0.72, None),
-    'entrance': Action('entrance', 0.72, None),
-    'diningroom': Action('diningroom', 0.72, None),
-    'laundry': Action('laundry', 0.72, None),
-    'thank you': Action('thanks', 0.72, None),
-}
+    This function parse the commands set in the commands.json file into a dictionary.
+
+    The key is the command itself and values are stored in a namedtuple called `Action`.
+
+    {
+        'do the thing': Action(topic='light', score=0.72, payload=None),
+        'bedroom': Action(topic='bedroom', score=0.72, payload=None),
+    }
+
+    Returns:
+        (dict): A dictionary with all the voice commands to be triggered by the assistant.
+
+    """
+    Action = namedtuple('Action', 'topic score payload')
+    with open('./commands.json', 'r') as file_:
+        raw_commands = json.load(file_)
+
+    commands = {key: Action(**value) for key, value in raw_commands.items()}
+
+    return commands
+
 
 def get_mic():
+    """Get the microphone stream
+
+    See the pyaudio library documentation for more info.
+    https://people.csail.mit.edu/hubert/pyaudio/
+
+    Return:
+        The microphone object.
+    """
     audio_devices = PyAudio()                 
     mic_stream = audio_devices.open(format=paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+    mic_stream.start_stream()
 
     return mic_stream
 
 
 def process_audio(recognizer, mic_stream):
+    """
+    Input chunks of the microphone stream into the speech model and output text.
+
+    Args:
+        recognizer: The KaldiRecognizer object from vosk.
+        mic_stream: The microphone stream from get_mic().
+
+    Returns:
+        A string or None
+
+    """
     audio_chunk = mic_stream.read(4096)
     if recognizer.AcceptWaveform(audio_chunk):
         text = json.loads(recognizer.Result())
-        if text['text'] and text['text'] != 'huh':
+        if text['text'] and text['text'] != 'huh':  # Filtering out 'huh' coming from noise.
             logger.info('Audio Process: %s' % text['text'])
             return (text['text'])
     
     return None
 
 
-def find_pattern(text):  
+def find_pattern(text):
+    """  
+    Compare the input text with the commands in `commands` dictionary.
+    It generates a score for each command.
+    The score defines how similar the text is to the command. 
+    See https://jamesturk.github.io/jellyfish/ for more info.
+
+    The commands are then sorted by score. If the highest score is equal or above
+    its score threshold it is selected and return as a Pattern namedtuple.
+    Example: 
+        Pattern(input='Zhu li do the thing', action='do the thing', score=1)
+
+    If it does not find any match, it also returns a Pattern object, but the action is set to `None`
+    and the score get a dictionary with a score for each available command.
+
+    Args:
+        text (str): string from process_audio()
+
+    Return:
+        (:obj:`namedtuple`): A Pattern namedtuple
+
+    """
+    Pattern = namedtuple('Pattern', 'input action score')
+      
     analyze_store = {}
     for key in commands.keys():    
         string_match = ASSISTANT_NAME + ' ' + key
@@ -76,64 +132,102 @@ def find_pattern(text):
     logger.info(msg)    
 
     if analyze_sorted[first_element][1] >= commands[first_element].score:
-        return Patern(text, first_element, analyze_sorted[first_element][1])
+        return Pattern(text, first_element, analyze_sorted[first_element][1])
     
-    return Patern(text, None, analyze_sorted)
+    return Pattern(text, None, analyze_sorted)
 
 
-def set_playload(patern):
-    playload = commands[patern.action].playload
-    playload_extra = {
-        'action': f'{ASSISTANT_NAME} {patern.action}',
-        'input': patern.input,
-        'score': patern.score
+def set_payload(pattern):
+    """ Build payload
+
+    This function is used to build the payload to be send with the MQTT message.
+    It creates a dictionary with the `action`'s name, the `input` from the speech to text & the matching `score` it got.
+    It also appends the payload set in the `commands.json` file.
+    
+    Args:
+        pattern (:obj:`namedtuple`): A Pattern namedtuple.
+
+    Return:
+        (dict): Final payload in a dictionary.
+
+    """
+    payload = commands[pattern.action].payload
+    payload_extra = {
+        'action': f'{ASSISTANT_NAME} {pattern.action}',
+        'input': pattern.input,
+        'score': pattern.score
     }
 
-    if playload is None:
-        playload = playload_extra
+    if payload is None:
+        payload = payload_extra
     else:
-        playload.update(playload_extra)
+        payload.update(payload_extra)
 
-    return playload
+    return payload
 
 
-def trigger_action(mqtt_client, patern):
-    action = commands[patern.action]
-    logger.info('%s %s Triggered' % (ASSISTANT_NAME, patern.action))
+def trigger_action(mqtt_client, pattern):
+    """Send a MQTT message succeeding match
+
+    Function used when a match is triggered.
+    The mqtt topic is build with the assistant's name and the action's topic. e.g: `zhuli/light`.
+
+    Attrs:
+        mqtt_client: A mqtt connection object. 
+        pattern (:obj:`namedtuple`): A Pattern namedtuple.
+
+    """
+        
+    action = commands[pattern.action]
+    logger.info('%s %s Triggered' % (ASSISTANT_NAME, pattern.action))
     mqtt_topic = ASSISTANT_NAME.lower().replace(' ', '') + '/' + action.topic
 
-    playload = set_playload(patern)
+    payload = set_payload(pattern)
 
-    mqtt_client.publish(mqtt_topic, payload=json.dumps(playload), qos=1)
+    mqtt_client.publish(mqtt_topic, payload=json.dumps(payload), qos=1)
 
 
-def trigger_fail(mqtt_client, patern):
-    playload = {'input': patern.input}
-    playload.update({'scores': patern.score})
+def trigger_fail(mqtt_client, pattern):
+    """Send a MQTT message on failing match
+
+    Function used when a match fails.
+    The mqtt topic is build with the assistant's name. e.g: `zhuli/fail`.
+    With also dump all the commands' scores in it. 
+    It can be useful if you want to keep some logs.
+
+    Attrs:
+        mqtt_client: A mqtt connection object.
+        pattern (:obj:`namedtuple`): A Pattern namedtuple.
+
+    """
+    payload = {'input': pattern.input}
+    payload.update({'scores': pattern.score})
 
     mqtt_failed_topic = ASSISTANT_NAME.lower().replace(' ', '') + '/' + 'fail'
 
-    mqtt_client.publish(mqtt_failed_topic, payload=json.dumps(playload), qos=1)
+    mqtt_client.publish(mqtt_failed_topic, payload=json.dumps(payload), qos=1)
 
 
 def main():
+    """Main loop.
+    """
     mic_stream = get_mic()
-    mic_stream.start_stream()
     recognizer = KaldiRecognizer(Model(MODEL), 16000)
 
     mqtt_client = mqtt.Client(client_id='', clean_session=True, userdata=None, protocol=mqtt.MQTTv311, transport='tcp')
     mqtt_client.connect(MQTT_SERVER, port=MQTT_PORT, keepalive=60)
-    mqtt_client.loop_start()
+    mqtt_client.loop_start()  # Needed to keep the mqtt connection alive.
 
     while True:
         try:
             text = process_audio(recognizer, mic_stream)
             if text:
-                patern = find_pattern(text)
-                if patern.action:
-                    trigger_action(mqtt_client, patern)
+                pattern = find_pattern(text)
+                if pattern.action:
+                    trigger_action(mqtt_client, pattern)
                 else:
-                    trigger_fail(mqtt_client, patern)
+                    if MSG_ON_FAIL:
+                        trigger_fail(mqtt_client, pattern)
 
         except KeyboardInterrupt:
             logger.warning('Terminating!')
@@ -150,4 +244,5 @@ def main():
 
 
 if __name__ == '__main__':
+    commands = fetch_commands()
     main()
